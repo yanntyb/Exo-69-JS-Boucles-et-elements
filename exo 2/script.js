d07a63d38664ca66798a78ece5c00ffa1ab86da0
yann tyb <yanntyb.lbc@gmail.com>
2020-12-02 16:03:12 +0100

    a

--- exo 2/script.js
+++ exo 2/script.js
@@ -1,9 +1,14 @@
 let liste = document.getElementById("liste-commissions");
 let input = document.getElementById("input");
 let bouton = document.getElementById("button");
+let boutonSupp = document.getElementById("buttonSupp");
 
 bouton.addEventListener("click",function (){
     let nouvelItem = document.createElement("li");
     nouvelItem.innerHTML = input.value;
     liste.appendChild(nouvelItem);
+})
+
+boutonSupp.addEventListener("click", function (){
+    liste.removeChild(liste.lastChild)
 })
